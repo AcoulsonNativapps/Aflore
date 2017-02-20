adb4f8e66d850f738f6b69c13b90b9e28cf7c83c
acoulson <andresbobadillacoulson@gmail.com>
2017-02-20 16:50:55 -0500

    initial commit

--- cifintask.py
+++ cifintask.py
@@ -9,7 +9,7 @@ url = 'http://cifinpruebas.asobancaria.com/InformacionComercialactWS/services/In
 crendials = dict(username=user, password=password)
 t = HttpAuthenticated(**crendials)
 client = Client(url=url, transport=t)
-print client
+#print client
 parametrosConsulta = client.factory.create('{http://dto.infocomercial.cifin.asobancaria.com}ParametrosConsultaDTO')
 
 parametrosConsulta.codigoInformacion = '154'
@@ -17,7 +17,7 @@ parametrosConsulta.motivoConsulta = '24'
 parametrosConsulta.numeroIdentificacion = '1143356938'
 parametrosConsulta.tipoIdentificacion ='1'
 
-print parametrosConsulta
+#print parametrosConsulta
 
 list_of_methods = [method for method in client.wsdl.services[0].ports[0].methods]
 method = client.wsdl.services[0].ports[0].methods[list_of_methods[0]]
@@ -25,5 +25,7 @@ method = client.wsdl.services[0].ports[0].methods[list_of_methods[0]]
 #print list_of_methods
 #print method.binding.input.param_defs(method)
 
-res = client.service.consultaXml(parametrosConsulta)
-print res
+#res = client.service.consultaXml(parametrosConsulta)
+#print res
+
+

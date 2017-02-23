4a82ce19e9528ec5101f2b81c01d342e65778ab3
acoulson <andresbobadillacoulson@gmail.com>
2017-02-23 17:15:31 -0500

    modificando todo da error al firmar

--- other.py
+++ other.py
@@ -3,7 +3,15 @@ import xml.dom.minidom
 import hashlib, datetime, email, os, sys, time, traceback
 from pysimplesoap.client import SoapClient, SimpleXMLElement
 from pysimplesoap.wsse import BinaryTokenSignature
+from suds.transport.https import HttpAuthenticated
 
+
+
+import logging
+logging.basicConfig(level=logging.INFO)
+#logging.getLogger('suds.client').setLevel(logging.DEBUG)
+#logging.getLogger('suds.transport').setLevel(logging.DEBUG)
+#logging.getLogger('suds.wsdl').setLevel(logging.DEBUG)
 
 
 
@@ -11,83 +19,45 @@ user = '90681'
 password = 'H33std'
 WSDL = 'http://cifinpruebas.asobancaria.com/InformacionComercialactWS/services/InformacionComercial?wsdl'
 crendials = dict(username=user, password=password)
+t = HttpAuthenticated(**crendials)
+location='http://cifinpruebas.asobancaria.com/InformacionComercialactWS/services/InformacionComercial'
+keyfile='key_nopass.pem'
+cert='cert.pem'
+headers = {"Content-TYpe" : "text/xml;charset=UTF-8",
+           "SOAPAction" : ""}
 
 
-CERT = "aflore.crt"
-PRIVATEKEY = "aflore.key"
-PASSPHRASE = "pwd123"
-import urllib2 as u2
 from suds.client import Client
-from suds.transport.http import HttpTransport, Reply, TransportError
-import httplib
+from suds.wsse import Security, Timestamp
+from wsse.suds import WssePlugin
 
-class HTTPSClientAuthHandler(u2.HTTPSHandler):
-    def __init__(self, key, cert):
-        u2.HTTPSHandler.__init__(self)
-        self.key = key
-        self.cert = cert
+def get_client(our_keyfile_path, our_certfile_path, their_certfile_path):
+    wsse = Security()
+    wsse.tokens.append(Timestamp())
 
-    def https_open(self, req):
-        # Rather than pass in a reference to a connection class, we pass in
-        # a reference to a function which, for all intents and purposes,
-        # will behave as a constructor
-        return self.do_open(self.getConnection, req)
-
-    def getConnection(self, host, timeout=300):
-        return httplib.HTTPSConnection(host, key_file=self.key,
-                                       cert_file=self.cert)
-
-class HTTPSClientCertTransport(HttpTransport):
-    def __init__(self, key, cert, proxy_settings=None, *args, **kwargs):
-        HttpTransport.__init__(self, *args, **kwargs)
-        self.key = key
-        self.cert = cert
-        self.proxy_settings = proxy_settings
-
-    def u2open(self, u2request):
-        """
-        Open a connection.
-        @param u2request: A urllib2 request.
-        @type u2request: urllib2.Requet.
-        @return: The opened file-like urllib2 object.
-        @rtype: fp
-        """
-        tm = self.options.timeout
-
-        https_client_auth_handler = HTTPSClientAuthHandler(self.key,
-                                                           self.cert)
-
-        # Add a proxy handler if the proxy settings is specified.
-        # Otherwise, just use the HTTPSClientAuthHandler.
-        if self.proxy_settings:
-            proxy_handler = u2.ProxyHandler(self.proxy_settings)
-            url = u2.build_opener(proxy_handler, https_client_auth_handler)
-        else:
-            url = u2.build_opener(https_client_auth_handler)
-
-        url = u2.build_opener()
-
-        if self.u2ver() < 2.6:
-            socket.setdefaulttimeout(tm)
-            return url.open(u2request)
-        else:
-            return url.open(u2request, timeout=tm)
-
-# Test #
+    return Client(
+        WSDL,
+        transport=t,
+        location=location,
+        wsse=wsse,
+        headers=headers,
+        plugins=[
+            WssePlugin(
+                keyfile=our_keyfile_path,
+                certfile=our_certfile_path,
+                their_certfile=their_certfile_path,
+            ),
+        ],
+    )
 if __name__ == '__main__':
-    key= 'key_nopass.pem'
-    cert = 'key.pem'
-    proxy_settings = {'https': 'http://90681:H33std@http://cifinpruebas.asobancaria.com:80'}
-    transport = HTTPSClientCertTransport(key, cert, proxy_settings)
 
 
-    client = Client(WSDL, transport=transport)
+    client = get_client(keyfile,cert,cert)
+
     parametrosConsulta = client.factory.create('{http://dto.infocomercial.cifin.asobancaria.com}ParametrosConsultaDTO')
 
     parametrosConsulta.codigoInformacion = '154'
     parametrosConsulta.motivoConsulta = '24'
     parametrosConsulta.numeroIdentificacion = '1143356938'
     parametrosConsulta.tipoIdentificacion = '1'
-    print client
-    method = getattr(client.service, 'consultaXml')
-    print method(parametrosConsulta)
+    print client.service.consultaXml(parametrosConsulta)
--- prueba.py
+++ prueba.py
@@ -1,8 +1,7 @@
-from zeep import Client
-from zeep.wsse.signature import Signature
-from zeep.transports import Transport
-from requests.auth import HTTPBasicAuth
-from requests import Session
+import hashlib, datetime, email, os, sys, time, traceback
+from pysimplesoap.client import SoapClient, SimpleXMLElement
+from pysimplesoap import xmlsec
+from M2Crypto import BIO, Rand, SMIME, SSL  # openssl binding
 
 
 user = '90681'
@@ -10,29 +9,109 @@ password = 'H33std'
 url = 'http://cifinpruebas.asobancaria.com/InformacionComercialWS/services/InformacionComercial?wsdl'
 location='http://cifinpruebas.asobancaria.com/InformacionComercialactWS/services/InformacionComercial'
 crendials = dict(username=user, password=password)
-session = Session()
-session.auth = HTTPBasicAuth(user, password)
+# Constants
+CERT = "cert.pem"  # X.509 certificate (in PEM format)
+PRIVATEKEY = "key_nopass.key"  # RSA private key (in PEM format)
+PASSPHRASE = "pwd123"  # private key password (if any)
+
+# Webservice URL (test: homologacion):
+WSDL = {'test': "https://secure.aduana.gov.py/test/wsaa/server?wsdl",
+        'prod': "https://secure.aduana.gov.py/wsaaserver/Server?wsdl"}
+
+# Remote webserver certificate validation, needed for "secure channel" spec
+CACERT = None  # WSAA CA Cert (Autoridades de Confiaza)
+
+DEFAULT_TTL = 60 * 60 * 5  # five hours
+TIMEOUT = 60  # 60 seconds for http connection timeout
+DEBUG = True
 
 
+def create_tra(service=None, ttl=2400, cert=None):
+    "Create a Access Request Ticket (TRA)"
+    # Base TRA squeleton (Ticket de Requerimiento de Acceso)
+    tra = SimpleXMLElement(
+        '<?xml version="1.0" encoding="UTF-8"?>'
+        '<loginTicketRequest version="1.0">'
+        '</loginTicketRequest>')
+    tra.add_child('header')
+    # get the source from the certificate subject, ie "CN=empresa, O=dna, C=py"
+    if cert:
+        crt = xmlsec.x509_parse_cert(cert)
+        tra.header.add_child('source', crt.get_subject().as_text())
+    tra.header.add_child('destination', 'C=py, O=dna, OU=sofia, CN=wsaatest')
+    d = int(time.mktime(datetime.datetime.now().timetuple()))
+    tra.header.add_child('uniqueId', str(d))
+    date = lambda ts: datetime.datetime.fromtimestamp(ts).isoformat()
+    tra.header.add_child('generationTime', str(date(d - ttl)))
+    tra.header.add_child('expirationTime', str(date(d + ttl)))
+    tra.add_child('service', service)
+    return tra.as_xml()
 
-client = Client(
- url,
-    transport=Transport(session=session),
-    wsse=Signature(
-    'aflore.key', 'key_nopass.pem',
-    'pwd123'))
 
-print client.wsdl.dump()
+def sign_tra(tra, cert=CERT, privatekey=PRIVATEKEY, passphrase=""):
+    "Sign using PKCS#7 the TRA and return CMS (trimming SMIME headers)"
 
-parametroCon = client.get_type('ns0:ParametrosConsultaDTO')
-strin = client.get_type('ns1:string')
-codigoInformacion = strin('154')
-motivoConsulta = strin('24')
-numeroIdentificacion = strin('1143356938')
-tipoIdentificacion = strin('1')
+    # Sign the text (tra) using m2crypto (openssl bindings for python)
+    buf = BIO.MemoryBuffer(tra)  # create the buffer from the file
+    # Rand.load_file('randpool.dat', -1)     # seed the PRNG
+    s = SMIME.SMIME()  # instantiate the SMIME
+    # support encription passwords (for private key, optional)
+    callback = lambda *args, **kwarg: passphrase
+    # load the private key and certificate
+    s.load_key(privatekey, cert, callback)  # (frmo file)
+    p7 = s.sign(buf, 0)  # Sign the buffer
+    out = BIO.MemoryBuffer()  # Instantiathe the output buffer
+    s.write(out, p7)  # Generate p7 in mail format
+    # Rand.save_file('randpool.dat')         # Store the PRNG's state
+
+    # extract the message body (signed part)
+    msg = email.message_from_string(out.read())
+    for part in msg.walk():
+        filename = part.get_filename()
+        if filename == "smime.p7m":  # is the signed part?
+            return part.get_payload(decode=False)  # return the CMS
 
 
-print client.service.consultaXml(parametroCon(codigoInformacion=codigoInformacion,
-                                              motivoConsulta=motivoConsulta,
-                                              numeroIdentificacion=numeroIdentificacion,
-                                              tipoIdentificacion=tipoIdentificacion))
+def call_wsaa(cms, wsdl=WSDL, proxy=None, cache=None, wrapper="", trace=False):
+    "Call the RPC method with the CMS to get the authorization ticket (TA)"
+
+    # create the webservice client
+    client = SoapClient(
+        location=wsdl[:-5],  # location, use wsdl,
+        cache=cache,
+        # proxy = parse_proxy(proxy),
+        # cacert = cacert,
+        timeout=TIMEOUT,
+        ns="ejb",
+        # TODO: find a better method to not include ns prefix in children:
+        #   (wsdl parse should detect qualification instead of server dialect)
+        soap_server="jetty",
+        namespace="http://ejb.server.wsaa.dna.gov.py/",
+        soap_ns="soapenv",
+        trace=trace)
+    # fix the wrong location (192.4.1.39:8180 in the WDSL)
+    ##ws = client.services['WsaaServerBeanService']
+    ##location = ws['ports']['WsaaServerBeanPort']['location']
+    ##location = location.replace("192.4.1.39:8180", "secure.aduana.gov.py")
+    ##ws['ports']['WsaaServerBeanPort']['location'] = wsdl[:-5] #location
+
+    # call the remote method
+    try:
+        results = client.loginCms(arg0=str(cms))
+    except:
+        # save sent and received messages for debugging:
+        open("request.xml", "w").write(client.xml_request)
+        open("response.xml", "w").write(client.xml_response)
+        raise
+
+    # extract the result:
+    ta = results['return'].encode("utf-8")
+    return ta
+
+
+if __name__ == "__main__":
+    tra = create_tra(service="test", ttl=DEFAULT_TTL, cert=CERT)
+    print tra
+    cms = sign_tra(tra, CERT, PRIVATEKEY)
+    ta = call_wsaa(cms,  url, trace=True)
+    print ta
